edc756893f31251118dd8661ad99eb5f9cd43481
Courbeau <korpelanaaro@gmail.com>
2020-07-27 15:29:30 +0300

    jaetaan yritys sivu kahtia

--- public/app.js
+++ public/app.js
@@ -27,11 +27,9 @@ const TuotteetSivu = {
             </nav>
         </div>
         </header>
-        <section class="section">
         <h2 class="otsikko">TUOTTEET</h2>
         <section class="kaikki-tuotteet-container">
           ${window.tuotteet?.map(TuotteetSivu.renderTuote).join('')}
-        </section>
         </section>
         `;
     }
@@ -42,7 +40,7 @@ const MessageForm = {
       return `
       <div class="formin-tausta">
         <form class="gform" method="POST" action="https://script.google.com/macros/s/AKfycbyPACzMbLllY4o7PuntVAavJ7tasgSHNvQ39q7U/exec">
-          <h3 class="Form-otsikko">Lähetä viesti</h3>
+          <h3 class="Form-otsikko">Lähetä meille viestiä</h3>
           <hr class="form-hr">
           <div class="nimi-mail-box">
             <input type="text" id="name" name="name" required placeholder="Nimi/nimimerkki">
@@ -81,23 +79,28 @@ const YrityksestäSivu = {
             </nav>
         </div>
         </header>
-        <section class="section">
-        <h2 class="otsikko">MITEN KAIKKI ALKOI</h2>
-        <div class="content-text">
-            <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper mauris nulla, id dapibus ante lobortis non. Integer hendrerit turpis lectus, in congue metus volutpat nec. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam cursus convallis erat, a auctor augue euismod a. Duis accumsan sem et scelerisque dignissim. Integer dictum bibendum tellus, vel ullamcorper libero euismod tincidunt. Nunc mollis metus a lacus tristique, sed suscipit orci lobortis. Morbi et rhoncus mi. Fusce mi nunc, iaculis mollis arcu in, posuere condimentum neque. Proin vitae neque tempus, scelerisque tortor ut, convallis neque. Nam a feugiat nulla, feugiat volutpat nulla. Proin malesuada magna in ultricies accumsan. Etiam vitae tristique quam. Duis commodo condimentum enim, non euismod arcu. Suspendisse vitae arcu pulvinar, dictum orci non, venenatis urna. Proin vestibulum sodales odio in maximus.</p>
-        </div>
-        <div class="kaikki-iconsit-kaikki-muut">
-          <div class="iconi-muu puhelin_y-tunnus">
-            <a href="tel:000000000"><img class="icon puhelin-icon" src="pictures/phone.png" alt="puhelinnumero"></a>
-            <p class="y-tunnus">Y-tunnus</p>
+        <div class="koko-yrityksesta-sivu">
+          <div class="yrityksesta-kuva"></div>
+          <div class="yrityksesta-sivu">
+            <h2 class="otsikko">MITEN KAIKKI ALKOI</h2>
+            <div class="content-text">
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper mauris nulla, id dapibus ante lobortis non. Integer hendrerit turpis lectus, in congue metus volutpat nec. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam cursus convallis erat, a auctor augue euismod a. Duis accumsan sem et scelerisque dignissim. Integer dictum bibendum tellus, vel ullamcorper libero euismod tincidunt. Nunc mollis metus a lacus tristique, sed suscipit orci lobortis. Morbi et rhoncus mi. Fusce mi nunc, iaculis mollis arcu in, posuere condimentum neque. Proin vitae neque tempus, scelerisque tortor ut, convallis neque. Nam a feugiat nulla, feugiat volutpat nulla. Proin malesuada magna in ultricies accumsan. Etiam vitae tristique quam. Duis commodo condimentum enim, non euismod arcu. Suspendisse vitae arcu pulvinar, dictum orci non, venenatis urna. Proin vestibulum sodales odio in maximus.</p>
+            </div>
+            <div class="kaikki-iconsit-kaikki-muut">
+              <div class="iconi-muu puhelin_y-tunnus">
+                <a href="tel:000000000"><img class="icon puhelin-icon" src="pictures/phone.png" alt="puhelinnumero"></a>
+                <p class="y-tunnus">Y-tunnus</p>
+              </div>
+              <div class="iconi-muu s-posti&osite">
+                <a href="mailto:john@example.com"><img class="icon s-posti-icon" src="pictures/mail.png" alt="s-posti"></a>
+                <p class="Osoite">Osoite</p>
+              </div>
+            </div>
+            <h1 class="company-name">Resta Turku OY</h1>
+            ` + MessageForm.render() + `
           </div>
-          <div class="iconi-muu s-posti&osite">
-            <a href="mailto:john@example.com"><img class="icon s-posti-icon" src="pictures/mail.png" alt="s-posti"></a>
-            <p class="Osoite">Osoite</p>
-          </div>
-        </div>
-        </section>
-        ` + MessageForm.render();
+        </div>`
+          
     }
 
   } 
